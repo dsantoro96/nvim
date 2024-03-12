99e3b6aedfa6c3c899c00de071eb4ef4ce3931dd
Daniele Santoro <daniele.santoro.96@gmail.com>
2024-03-12 20:21:01 +0100

    Fix cmp

--- lua/zvott/plugins/lsp.lua
+++ lua/zvott/plugins/lsp.lua
@@ -8,7 +8,6 @@ return {
             { 'hrsh7th/cmp-nvim-lsp' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-cmdline' },
             { 'L3MON4D3/LuaSnip' },
             { 'saadparwaiz1/cmp_luasnip' },
             { 'rafamadriz/friendly-snippets' },
@@ -17,6 +16,36 @@ return {
         config = function()
             require('fidget').setup({})
             require('mason').setup()
+
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            local luasnip = require("luasnip")
+            luasnip.config.setup({})
+
+            require('luasnip.loaders.from_vscode').lazy_load()
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end
+                },
+                completion = { completeopt = 'menu,menuone,noinsert' },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    -- { name = 'luasnip' },
+                    { name = 'path' },
+                }, {
+                    { name = 'buffer' },
+                })
+            })
 
             local cmp_lsp = require('cmp_nvim_lsp')
             local capabilities = vim.tbl_deep_extend(
@@ -46,7 +75,7 @@ return {
                     ["lua_ls"] = function()
                         local lspconfig = require("lspconfig")
 
-                        lspconfig.lua_ls.setup {
+                        lspconfig.lua_ls.setup({
                             capabilities = capabilities,
                             settings = {
                                 Lua = {
@@ -55,34 +84,9 @@ return {
                                     }
                                 }
                             }
-                        }
+                        })
                     end,
                 }
-            })
-
-            local cmp = require('cmp')
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-            require('luasnip.loaders.from_vscode').lazy_load()
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim-lsp' },
-                    { name = 'luasnip' }
-                }, {
-                    { name = 'buffer' },
-                })
             })
 
             vim.diagnostic.config({
--- lua/zvott/plugins/trouble.lua
+++ lua/zvott/plugins/trouble.lua
@@ -8,6 +8,7 @@ return {
         })
 
         vim.keymap.set('n', '<leader>tt', function() trouble.toggle() end)
+        vim.keymap.set('n', '<leader>tq', function() trouble.toggle('quickfix') end)
         vim.keymap.set('n', '<leader>tn', function() trouble.next({ skip_groups = true, jump = true }) end)
         vim.keymap.set('n', '<leader>tp', function() trouble.previous({ skip_groups = true, jump = true }) end)
     end
