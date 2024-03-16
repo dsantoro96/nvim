9858f1582b3ade5ce0bb49b75ed25d73ed5cc814
Daniele Santoro <daniele.santoro.96@gmail.com>
2024-03-16 16:56:45 +0100

    Added python lsp

--- lua/zvott/plugins/lsp.lua
+++ lua/zvott/plugins/lsp.lua
@@ -64,6 +64,7 @@ return {
                     'tsserver',
                     'eslint',
                     'clangd',
+                    'pylsp',
                 },
 
                 handlers = {
