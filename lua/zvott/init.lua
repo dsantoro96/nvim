7c885b7708d78d4042db1486ef7f4d7ea53d6737
Daniele Santoro <daniele.santoro.96@gmail.com>
2024-06-09 17:27:22 +0200

    Added Java LSP

--- lua/zvott/init.lua
+++ lua/zvott/init.lua
@@ -19,6 +19,7 @@ autocmd('LspAttach', {
         local opts = { buffer = e.buf }
 
         vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
         vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
--- lua/zvott/plugins/lsp.lua
+++ lua/zvott/plugins/lsp.lua
@@ -12,6 +12,7 @@ return {
             { 'saadparwaiz1/cmp_luasnip' },
             { 'rafamadriz/friendly-snippets' },
             { 'j-hui/fidget.nvim' },
+            { 'mfussenegger/nvim-jdtls' },
         },
         config = function()
             require('fidget').setup({})
@@ -55,27 +56,35 @@ return {
                 cmp_lsp.default_capabilities()
             )
 
+            local lspconfig = require("lspconfig")
+
             require('mason-lspconfig').setup({
                 ensure_installed = {
-                    'angularls',
                     'tailwindcss',
                     'lua_ls',
                     'yamlls',
                     'tsserver',
-                    'eslint',
                     'clangd',
                     'pylsp',
+                    'jdtls',
                 },
 
                 handlers = {
                     function(server_name) -- default handler (optional)
-                        require("lspconfig")[server_name].setup({
+                        lspconfig[server_name].setup({
                             capabilities = capabilities,
                         })
                     end,
+                    ['clangd'] = function()
+                        lspconfig.clangd.setup({
+                            capabilities = capabilities,
+                            cmd = {
+                                'clangd',
+                                '--fallback-style=webkit',
+                            },
+                        })
+                    end,
                     ["lua_ls"] = function()
-                        local lspconfig = require("lspconfig")
-
                         lspconfig.lua_ls.setup({
                             capabilities = capabilities,
                             settings = {
@@ -86,6 +95,76 @@ return {
                                 }
                             }
                         })
+                    end,
+                    ["jdtls"] = function()
+                        local home = os.getenv 'HOME'
+                        local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+                        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+                        local workspace_dir = workspace_path .. project_name
+
+                        local status, jdtls = pcall(require, 'jdtls')
+                        if not status then
+                            return
+                        end
+                        local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+                        local config = {
+                            cmd = {
+                                'java',
+                                '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                                '-Dosgi.bundles.defaultStartLevel=4',
+                                '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                                '-Dlog.protocol=true',
+                                '-Dlog.level=ALL',
+                                '-Xmx1g',
+                                '--add-modules=ALL-SYSTEM',
+                                '--add-opens',
+                                'java.base/java.util=ALL-UNNAMED',
+                                '--add-opens',
+                                'java.base/java.lang=ALL-UNNAMED',
+                                '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+                                '-jar',
+                                vim.fn.glob(home ..
+                                '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+                                '-configuration',
+                                home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+                                '-data',
+                                workspace_dir,
+                            },
+                            root_dir = function ()
+                                return require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' }
+                            end,
+
+                            settings = {
+                                java = {
+                                    signatureHelp = { enabled = true },
+                                    extendedClientCapabilities = extendedClientCapabilities,
+                                    maven = {
+                                        downloadSources = true,
+                                    },
+                                    referencesCodeLens = {
+                                        enabled = true,
+                                    },
+                                    references = {
+                                        includeDecompiledSources = true,
+                                    },
+                                    inlayHints = {
+                                        parameterNames = {
+                                            enabled = 'all', -- literals, all, none
+                                        },
+                                    },
+                                    format = {
+                                        enabled = false,
+                                    },
+                                },
+                            },
+
+                            init_options = {
+                                bundles = {},
+                            },
+                            capabilities = capabilities,
+                        }
+                        lspconfig.jdtls.setup(config)
                     end,
                 }
             })
